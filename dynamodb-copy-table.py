72f0adf434ed4978e369aff57aebd63d921318ec
Ameya Joshi <v-ameyaj@zillowgroup.com>
2021-08-03 19:18:36 +0530

    Updated to boto3 and added sessions for source and destination

--- dynamodb-copy-table.py
+++ dynamodb-copy-table.py
@@ -1,37 +1,43 @@
 #!/usr/bin/env python
-from boto.dynamodb2.exceptions import ValidationException
-from boto.dynamodb2.fields import HashKey, RangeKey
-from boto.dynamodb2.layer1 import DynamoDBConnection
-from boto.dynamodb2.table import Table
-from boto.exception import JSONResponseError
-from time import sleep
 import sys
 import os
+from time import sleep
+
+import boto3
 
 if len(sys.argv) != 3:
-    print("Usage: %s <source_table_name> <destination_table_name>"% sys.argv[0])
+    print("Usage: %s <source_table_name> <destination_table_name>" % sys.argv[0])
     sys.exit(1)
 
 src_table = sys.argv[1]
 dst_table = sys.argv[2]
 region = os.getenv('AWS_DEFAULT_REGION', 'us-west-2')
+source_profile = os.getenv('AWS_SOURCE_PROFILE', 'default')
+destination_profile = os.getenv('AWS_DESTINATION_PROFILE', source_profile)
 
-# host = 'dynamodb.%s.amazonaws.com' % region
-# ddbc = DynamoDBConnection(is_secure=False, region=region, host=host)
-DynamoDBConnection.DefaultRegionName = region
-ddbc = DynamoDBConnection()
+print("Source profile ", source_profile)
+print("Destination profile ", destination_profile)
+
+source_session = boto3.session.Session(profile_name=source_profile, region_name=region)
+ddbsc = source_session.client("dynamodb")
+if source_profile == destination_profile:
+    ddbdc = ddbsc
+else:
+    destination_session = boto3.session.Session(profile_name=destination_profile, region_name=region)
+    ddbdc = destination_session.client("dynamodb")
+
 
 # 1. Read and copy the target table to be copied
 table_struct = None
 try:
-    logs = Table(src_table, connection=ddbc)
-    table_struct = logs.describe()
-except JSONResponseError:
+    table_struct = ddbsc.describe_table(TableName=src_table)
+    print("table struct ", table_struct)
+except ddbsc.exceptions.ResourceNotFoundException:
     print("Table %s does not exist" % src_table) 
     sys.exit(1)
 
 print("*** Reading key schema from %s table" % src_table)
-src = ddbc.describe_table(src_table)['Table']
+src = table_struct['Table']
 hash_key = ''
 range_key = ''
 for schema in src['KeySchema']:
@@ -41,43 +47,70 @@ for schema in src['KeySchema']:
         hash_key = attr_name
     elif key_type == 'RANGE':
         range_key = attr_name
+print("source schema ", src['KeySchema'])
+
+print("hash key ", hash_key)
+print("range key ", range_key)
 
 # 2. Create the new table
 table_struct = None
 try:
-    new_logs = Table(dst_table,
-                    connection=ddbc,
-                    schema=[HashKey(hash_key),
-                            RangeKey(range_key),
-                            ]
-                    )
-
-    table_struct = new_logs.describe()
+    table_struct = ddbdc.describe_table(TableName=dst_table)
+    print("dest table struct ", table_struct)
     if 'DISABLE_CREATION' in os.environ:
         print("Creation of new table is disabled. Skipping...")
     else:
         print("Table %s already exists" % dst_table)
         sys.exit(0)
-except JSONResponseError:
-    schema = [HashKey(hash_key)]
+except ddbdc.exceptions.ResourceNotFoundException:
+    schema = [
+                 {
+                        'AttributeName': hash_key,
+                        'KeyType': 'HASH'
+                    },
+
+                ]
+    attributeDefinitions = [
+        {
+            'AttributeName': hash_key,
+            'AttributeType': 'S'
+        },
+    ]
+
     if range_key != '':
-        schema.append(RangeKey(range_key))
-    new_logs = Table.create(dst_table,
-                            connection=ddbc,
-                            schema=schema,
-                            )
+        schema.append({
+                        'AttributeName': range_key,
+                        'KeyType': 'RANGE'
+                    })
+        attributeDefinitions.append({
+            'AttributeName': range_key,
+            'AttributeType': 'S'
+        })
+    billing_info = {'BillingMode': src['BillingModeSummary']['BillingMode']}
+    readCapacity = src['ProvisionedThroughput']['ReadCapacityUnits']
+    writeCapacity = src['ProvisionedThroughput']['WriteCapacityUnits']
+    if readCapacity > 0 and writeCapacity > 0:
+        billing_info['ProvisionedThroughput'] = {
+                                      'ReadCapacityUnits': src['ProvisionedThroughput']['ReadCapacityUnits'],
+                                      'WriteCapacityUnits': src['ProvisionedThroughput']['WriteCapacityUnits'],
+                                  }
+
+    new_logs = ddbdc.create_table(TableName=dst_table,
+                                  KeySchema=schema,
+                                  AttributeDefinitions=attributeDefinitions,
+                                  **billing_info
+                                  )
     print("*** Waiting for the new table %s to become active" % dst_table)
     sleep(5)
-    while ddbc.describe_table(dst_table)['Table']['TableStatus'] != 'ACTIVE':
+    while ddbdc.describe_table(TableName=dst_table)['Table']['TableStatus'] != 'ACTIVE':
         sleep(3)
-    
 
 if 'DISABLE_DATACOPY' in os.environ:
     print("Copying of data from source table is disabled. Exiting...")
     sys.exit(0)
 
 # 3. Add the items
-for item in logs.scan():
+for item in ddbsc.scan(TableName=src_table)['Items']:
     new_item = {}
     new_item[hash_key] = item[hash_key]
     if range_key != '':
@@ -87,11 +120,9 @@ for item in logs.scan():
             continue
         new_item[f] = item[f]
     try:
-        new_logs.use_boolean()
-        new_logs.put_item(new_item, overwrite=True)
-    except ValidationException:
+        print("new item ", new_item)
+        ddbdc.put_item(TableName=dst_table, Item=new_item)
+    except ddbdc.exceptions.ValidationException:
         print(dst_table, new_item)
-    except JSONResponseError:
-        print(ddbc.describe_table(dst_table)['Table']['TableStatus'])
 
 print("We are done. Exiting...")
